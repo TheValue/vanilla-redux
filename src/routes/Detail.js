e9614ebb69cac98a7dca3b82fc348c1780205cdb
CHO MIN SU <chominsu@CHOui-MacBookPro.local>
2024-01-08 13:31:29 +0900

    study..

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,20 @@
-export default () => "Detail";
+import React from "react";
+import { connect } from "react-redux";
+import { useParams } from "react-router-dom";
+
+function Detail({ toDos }) {
+    const myId = useParams().id;
+    const toDo = toDos.find((toDo) => toDo.id === parseInt(myId));
+    return (
+        <>
+            <h1>{toDo?.text}</h1>
+            <h5>Create at: {toDo?.id}</h5>
+        </>
+    )
+}
+
+function mapStateToProps(state) {
+    return {toDos: state}
+}
+
+export default connect(mapStateToProps) (Detail);
--- src/store.js
+++ src/store.js
@@ -1,34 +1,16 @@
-import {legacy_createStore as createStore} from "redux"
+import { configureStore, createSlice } from "@reduxjs/toolkit"
 
-const ADD = "ADD";
-const DELETE = "DELETE";
-
-export const addToDo = (text) => {
-    return {
-        type: ADD,
-        text
+const toDos = createSlice( {
+    name: 'toDosReducer',
+    initialState: [],
+    reducers: {
+        add: (state, action) => {
+            state.push({text: action.payload, id: Date.now()})
+        },
+        remove: (state, action) => state.filter(toDo => toDo.id !== action.payload)
     }
-}
+})
 
-export const deleteToDo = id => {
-    return {
-        type: DELETE,
-        id
-    }
-}
+export const {add, remove} = toDos.actions;
 
-const reducer = (state = [], action) => {
-    switch(action.type) {
-        case ADD:
-            return [{text: action.text, id: Date.now(), ...state}];
-        case DELETE: 
-            return state.filter(toDo => toDo.id !== action.id);
-        default:
-            return state;
-    }
-}
-const store = createStore(reducer);
-
-
-
-export default store;
+export default configureStore({ reducer: toDos.reducer });
